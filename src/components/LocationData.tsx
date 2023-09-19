f0ff0bf4947ce7ebb7204bc57c508bf0d9bda6e5
champs-8 <https://github.com/champs-8/Projetos>
2023-09-18 23:57:48 -0300

    adicionado função de buscar infos dos países

--- src/components/LocationData.tsx
+++ src/components/LocationData.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 import axios from "axios";
 
 export  function GetHourNow() {
@@ -28,8 +28,6 @@ export function GetLocationIp() {
         .then(response => setLocation(response.data))
         .catch(err => console.log(`erro ao buscar IP: ${err}`));
     }, []);
-    
-    console.log(location);
     
     return(
         <div>
