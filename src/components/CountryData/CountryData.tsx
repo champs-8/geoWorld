f35f4f562d75a1ca3f1d9931070210f872f74881
champs-8 <https://github.com/champs-8/Projetos>
2023-09-26 23:56:50 -0300

    informações dos países ja add

--- src/components/CountryData/CountryData.tsx
+++ src/components/CountryData/CountryData.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios";
 import { useState, useEffect } from "react";
 
@@ -7,25 +8,37 @@ export interface CountryProps  {
 }
 
 interface InfoCountryProps {
-    official: string,
-    independent: boolean
-    currencies: string,
+    name: {
+        official: string,
+    }
     capital: string,
-    region: string,
-    languages: string,
+    languages:{
+        [key: string]: any
+    },
     area: number,
-    maps: string,
     population: number,
-    flags: string
-
-
-
+    subregion: string,
+    currencies: {
+        [key: string]: {
+            name: string,
+            symbol: string
+        }
+    },
+    maps: {
+        googleMaps : string
+    },
+    independent: boolean
+    flags: {
+        png: string,
+        svg: string
+    }
 }
 
 
 export default function SearchCountry({country}: CountryProps) {
 
-    const [infoCountry, setInfoCountry] = useState<InfoCountryProps | null>(null)
+    const [infoCountry, setInfoCountry] = useState<InfoCountryProps[] | null>(null)
+    
 
     //URL base da API de informações dos países : https://restcountries.com/v3.1/all
 
@@ -36,23 +49,52 @@ export default function SearchCountry({country}: CountryProps) {
             try {
                 const response = await axios.get('https://restcountries.com/v3.1/name/'+country);
                 setInfoCountry(response.data)
-                console.log(response.data);
+                // console.log(response.data[0]);
+
             } catch(error) {
                 console.log(`Erro ao buscar o país ${country} : ${error}`);
                 setInfoCountry(null);
             }
         }
-        console.log(infoCountry);
 
         fetchCountryInfo();
-    }, [country])
+    }, [country]);
+
+
+    //quando atualizar o infoCountry, irá mostrar no clg
+    useEffect(() => {
+        console.log(infoCountry);
+    }, [infoCountry]);
+
 
     return (
         <div>
             {infoCountry ? (
                 <div>
                     <h2>{country}</h2>
-                    <p>Nome oficial: {infoCountry.official}</p>
+                    <p>Nome oficial: {infoCountry[0].name.official}</p>
+                    <p>Capital: {infoCountry[0].capital}</p>
+
+                    {/* buscando idiomas dinamicamente, ja que nao será sempre as mesmas propriedades */}
+
+                    {Object.entries(infoCountry[0].languages).map(([index ,value]) => (
+                        <p>Idioma {index}: {value}</p>
+                    ))}
+                    
+                    <p>Área: {(infoCountry[0].area).toLocaleString()} km²</p>
+                    <p>População: {(infoCountry[0].population).toLocaleString()}</p>
+                    <p>Região: {infoCountry[0].subregion}</p>
+
+                    {/* <p>Moedas: {infoCountry[0].currencies["key"].name}</p> */}
+                    <div>
+                        <p>Moedas: {Object.keys(infoCountry[0].currencies)} - {Object.keys(infoCountry[0].currencies).map((currencyKey) => (
+                            infoCountry[0].currencies[currencyKey].symbol
+                        ))}</p>
+                    </div>
+
+                    <p>Mapa: {infoCountry[0].maps.googleMaps}</p>
+                    <p>Independente: {infoCountry[0].independent}</p>
+                    <p>{infoCountry[0].flags.png}</p>
                 </div>
             ) : (
                 <p>
